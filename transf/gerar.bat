199d3e213e88c3013c4a6d287af142eda3386214
Guido <guido.gabriel1987@gmail.com>
2018-10-23 19:06:11 -0300

    backup

---
+++ transf/gerar.bat
@@ -0,0 +1,4 @@
+
+echo tesssssssssssssssste
+highcharts-export-server.cmd --infile D:\github\pdigram\transf\options.json --tmpdir D:\github\pdigram\transf\tmp\
+echo tesssssssssssssssste222222222222222222
